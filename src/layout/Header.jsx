6cd689d203f16cda0363140781233c4295c6ca85
Antonio Vazquez <54569464+tonyekssj@users.noreply.github.com>
2022-10-12 15:52:54 -0700

    arreglo de boton

---
+++ src/layout/Header.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import {MenuItems} from "./MenuItems";
+import {Button} from "../components/Button";
+import logovware from "../images/logo-vware.png";
+
+import './Header.css';
+
+
+class Header extends Component{
+  state = {clicked : false}
+  
+  handleClick = () => {
+    this.setState ({ clicked: !this.state.clicked})
+  }
+
+  render(){
+    return(
+      <nav className='HeaderItems'>
+        <h1 className="header-logo"><img src={logovware}></img></h1>
+        <div className='menu-icon' onClick={this.handleClick}>
+          <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+
+        </div>
+        <ul className={this.state.clicked ? 'header-menu active' :'header-menu'}>
+            {MenuItems.map((item, index) =>{
+              return(
+                <li key = {index}>
+                  <a className={item.cName} href={item.url}>
+                  {item.tittle}
+                  </a>      
+                </li>
+              )
+            })}
+        </ul>
+      <Button>Nosotros</Button>
+      </nav>
+    )
+  }
+}
+
+export default Header;
---
+++ src/layout/MenuItems.jsx
@@ -0,0 +1,14 @@
+export const MenuItems = [
+    {
+        tittle:'Inicio',
+        url:'../App.js',
+        cName: 'header-links'
+    },
+    {
+        tittle:'Nosotros',
+        url:'#',
+        cName: 'header-links-mobile'
+    },
+    
+
+]
