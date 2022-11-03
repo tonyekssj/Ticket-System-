03ec44852505ff7cb67121728d6214b11dff7107
Antonio Vazquez <54569464+tonyekssj@users.noreply.github.com>
2022-11-03 11:39:55 -0700

    Pagina de inicio LISTO
    login listo

--- src/pages/Nosotros.jsx
+++ src/pages/Nosotros.jsx
@@ -6,8 +6,22 @@ export const Nosotros =() =>{
     return(  
     <body>
         <div className="box-nosotros">
-            <p className="nosotros-titulo">Nosotros</p>
+        <p className="nosotros-titulo">Nosotros</p>
+        <p className="texto-nosotros">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut vestibulum est. In hac habitasse platea dictumst. Ut nec augue auctor, aliquet sem at, porta nisi. Nulla facilisis quis arcu sit amet condimentum. Nullam eu auctor ante. Suspendisse faucibus libero sed tristique dictum. Donec sit amet ligula accumsan, pharetra est facilisis, efficitur lectus. Integer ullamcorper bibendum enim id maximus. Maecenas laoreet sem in mi vestibulum, at bibendum dolor fermentum. Phasellus vestibulum ante ac sapien eleifend vulputate. Nulla quis elit ut leo pretium fringilla quis non massa. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+        <br/>
+        <br/>
+    
+        <table className="table-nosotros">
+            <tr>
+                <td className="nosotros-primero">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut vestibulum est. In hac habitasse platea dictumst. Ut nec augue auctor, aliquet sem at, porta nisi. Nulla facilisis quis arcu sit amet condimentum.</td>
+                <td className="nosotros-primero">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut vestibulum est. In hac habitasse platea dictumst. Ut nec augue auctor, aliquet sem at, porta nisi. Nulla facilisis quis arcu sit amet condimentum.</td>
+                <td className="nosotros-primero">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut vestibulum est. In hac habitasse platea dictumst. Ut nec augue auctor, aliquet sem at, porta nisi. Nulla facilisis quis arcu sit amet condimentum.</td>
+            </tr>
+        </table>
         </div>
+
+       
+        
     </body>
  
     )
